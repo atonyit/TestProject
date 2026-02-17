9edddabcb1d419c9feabd2359de59559c347b5ca
alan <alanitoyah96@gmail.com>
2026-02-17 00:35:05 -0600

    Finished input validation, added the ability to return the InputBoxes after submitting, and added more event handlers for UX

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-function validateInput(num1, num2){
-    //input validation for all 
+const inputOne = document.getElementById('inputOne');
+const inputTwo = document.getElementById('inputTwo');
+const calculateButton = document.getElementById('calculateButton');
+const resultOutput = document.getElementById('result');
 
-    // if (num2 == 0){
-    //     return console.error("Can't do that");
-    // }
-}
+//event listener to returnInputVisibility, clearInputs, and calculateTotal
+calculateButton.addEventListener("click", calculateTotal)
+
+document.addEventListener("keydown", function(event){
+    if(event.key == "Enter"){
+        calculateTotal()
+    }
+})
+document.addEventListener("dblclick", function(){
+    returnInputVisibility() 
+    clearInputs()
+})
+document.addEventListener("keydown", function(event) {
+    if (event.key == "Escape"){
+        returnInputVisibility()
+        clearInputs()
+    }
+})
 
 function calculateTotal(){
     //inputs have to be numbers
-    const inputOne = document.getElementById('inputOne').valueAsNumber
-    const inputTwo = document.getElementById('inputTwo').valueAsNumber
+    const num1 = inputOne.valueAsNumber
+    const num2 = inputTwo.valueAsNumber
 
-    // validateInput(inputOne, inputTwo)
+    if (!validateInput(num1, num2))
+        return
 
-
-    //division
-    const total = inputOne / inputTwo
-    console.log(total)
-
-    //display total
+    //only divide if input is valid
+    const total = num1 / num2
     displayTotal(total)
 }
 
 function displayTotal(total){
-    const inputOne = document.getElementById('inputOne')
-    const inputTwo = document.getElementById('inputTwo')
-    const calculateButton = document.getElementById('calculateButton')
-    const resultOutput = document.getElementById('result')
-
     //hide the input fields after calculation
     inputOne.style.display = 'none'
     inputTwo.style.display = 'none'
@@ -38,10 +46,43 @@ function displayTotal(total){
     resultOutput.textContent = total
 }
 
-//on any click/button change return inputs/calculate button
-function returnInputVisibility(){
+function validateInput(num1, num2){
+    //if either number isn't typed-in throw an error
+    if (Number.isNaN(num1) || Number.isNaN(num2)){
+        return showError("Please enter both numbers."), false
+    }
+    
+    //throw error if second number is 0
+    if (num2 == 0){
+        return showError("Numbers can't be divided by zero."), false
+    }
+
+    return true
 }
 
-//event listener to run calculateTotal
-const calculateButton = document.getElementById('calculateButton')
-calculateButton.addEventListener("click", calculateTotal)
+function showError(errorMessage){
+    //hide other elements
+    inputOne.style.display = 'none'
+    inputTwo.style.display = 'none'
+    calculateButton.style.display = 'none'
+
+    //display the result
+    resultOutput.style.display = 'block'
+    resultOutput.textContent = errorMessage
+}
+
+//on any dblclick/button press return inputs/calculate button
+function returnInputVisibility(){
+    inputOne.style.display = 'block'
+    inputTwo.style.display = 'block'
+    calculateButton.style.display = 'block'
+    
+    resultOutput.style.display = 'none'
+    resultOutput.textContent = ""
+    
+}
+
+function clearInputs(){
+    inputOne.value = ""
+    inputTwo.value = ""
+}
